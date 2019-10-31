05f8b8b13a761cf1a15ac308625a1f06ac2322c5
Mahim Agrawal <mahim23agrawal@gmail.com>
2019-10-31 12:09:38 +0530

    Fix stats_instance

--- lc_slave/stats_instance.sh
+++ lc_slave/stats_instance.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-DOCKER_STATS_CMD=`docker stats $1 --no-stream --format "table {{.MemPerc}} {{.CPUPerc}}"`
+DOCKER_STATS_CMD=`docker stats $1 --no-stream --format "table {{.MemPerc}} {{.CPUPerc}}" | awk 'NR==2{print}'`
 echo "${DOCKER_STATS_CMD}"
